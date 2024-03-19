fbb00e74e4441607755d281ace66ed873f234c0e
Shamshid <shmshd12@gmail.com>
2024-03-19 13:58:38 +0400

    Add docker compose with nginx and database

---
+++ database/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE todo (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL
+);
+
+INSERT INTO todo (title) VALUES ('Buy groceries');
+INSERT INTO todo (title) VALUES ('Go for a run');
